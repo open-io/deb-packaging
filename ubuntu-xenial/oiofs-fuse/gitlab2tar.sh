99e53c3b8592486bcc2fe44fe0d7a8736950b6f4
Romain ACCIARI <romain.acciari@gmail.com>
2016-10-08 10:50:18 +0000

    Updated script

--- ubuntu-xenial/oiofs-fuse/gitlab2tar.sh
+++ ubuntu-xenial/oiofs-fuse/gitlab2tar.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-cid=4e60775548fcddb2babf38fdd895039945e4630f
+#set -x
+
+giturl="git@gitlab.openio.io:openio/oio-fs.git"
+
+echo "###$1###"
+if [ -n "$1" -a "$1" != ' ' ]; then
+  cid=$1
+else
+  cid=master
+fi
 
 rm -vf oio-fs.tar.gz
 rm -vrf oio-fs
-#git clone -v --depth 1 git@gitlab.openio.io:openio/oio-fs.git
-#git clone --depth 1 git@gitlab.openio.io:jkasarherou/oio-fs.git
-git clone -n git@gitlab.openio.io:openio/oio-fs.git
+git clone -n $giturl
 pushd oio-fs
   git checkout $cid
   git submodule update --init
   rm -rf .git
+  if [ "$cid" == "master" ]; then
+    cid=$(git log --format="%H" -n 1)
+  fi
 popd
-tar zvcf oio-fs.tar.gz oio-fs
+tar zvcf oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz oio-fs
+rm -vrf oio-fs
+echo oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz  oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz >sources
