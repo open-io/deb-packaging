0194068eb39ab8eab9fad2fdb00545a3947793a4
Romain ACCIARI <romain.acciari@gmail.com>
2016-10-08 10:48:19 +0000

    Create sources file for packaging

--- debian-jessie/oiofs-fuse/gitlab2tar.sh
+++ debian-jessie/oiofs-fuse/gitlab2tar.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-cid=6be0fdcc97535bbb2363e7d08dd44c0792bfb962
+#set -x
+
+giturl="git@gitlab.openio.io:openio/oio-fs.git"
+
+echo "###$1###"
+if [ -n "$1" -a "$1" != ' ' ]; then
+  cid=$1
+else
+  cid=master
+fi
 
 rm -vf oio-fs.tar.gz
 rm -vrf oio-fs
-git clone -n git@gitlab.openio.io:openio/oio-fs.git
+git clone -n $giturl
 pushd oio-fs
   git checkout $cid
   git submodule update --init
   rm -rf .git
+  if [ "$cid" == "master" ]; then
+    cid=$(git log --format="%H" -n 1)
+  fi
 popd
 tar zvcf oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz oio-fs
 rm -vrf oio-fs
+echo oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz  oiofs-fuse_$(date +%Y%m%d%H%M).git${cid:0:7}.orig.tar.gz >sources
