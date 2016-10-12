4de483db7aea2cd7192c868cf4b7c5c69f7f068c
Romain ACCIARI <romain.acciari@gmail.com>
2016-10-12 14:43:50 +0000

    Initial release

---
+++ ubuntu-xenial/python-django-redis-cache/debian/python-django-redis-cache.default.ex
@@ -0,0 +1,10 @@
+# Defaults for python-django-redis-cache initscript
+# sourced by /etc/init.d/python-django-redis-cache
+# installed at /etc/default/python-django-redis-cache by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
