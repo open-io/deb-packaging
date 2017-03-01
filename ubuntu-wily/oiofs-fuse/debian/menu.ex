85a24e39002c70b06c76a5e86589abe4b38bd310
Romain ACCIARI <romain.acciari@gmail.com>
2017-03-01 14:55:01 +0000

    Remove ubuntu-wily support

--- ubuntu-wily/oiofs-fuse/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(oiofs-fuse):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="oiofs-fuse" command="/usr/bin/oiofs-fuse"
--- ubuntu-wily/openio-asn1c/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(openio-asn1c):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="openio-asn1c" command="/usr/bin/openio-asn1c"
