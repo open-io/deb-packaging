open-io/deb-packaging

commit 4e6794135fbee31bbb0fa36e247472e4e4dae38b

Add specific case for the ARM64 repository

oio-debbuild.sh
@@ -154,5 +154,8 @@ if [[ "${REPO}" =~ ^http:// ]]; then
     done
 else
     echo "### Uploading package $pkgdsc to repository ${OSDISTID}-openio-${REPO}"
-    dput -f -u ${OSDISTID}-openio-${REPO} /var/cache/pbuilder/${OSDISTID}-${OSDISTCODENAME}-${ARCH}/result/$(basename ${pkgdsc} .dsc)*.changes
+    if [ "${OSDISTID}" == 'ubuntu' -a "${ARCH}" == 'arm64' ]; then
+      dput -f -u ${OSDISTID}-arm64-openio-${REPO} /var/cache/pbuilder/${OSDISTID}-${OSDISTCODENAME}-${ARCH}/result/$(basename ${pkgdsc} .dsc)*.changes
+    else
+      dput -f -u ${OSDISTID}-openio-${REPO} /var/cache/pbuilder/${OSDISTID}-${OSDISTCODENAME}-${ARCH}/result/$(basename ${pkgdsc} .dsc)*.changes
 fi

commit 945a98acc7ec1e5399ba2962b1f4819030d53243

Fix syntax error in script (missing "fi")
Fixes: commit 4e6794135fbee31bbb0fa36e247472e4e4dae38b

oio-debbuild.sh
@@ -158,4 +158,5 @@ else
       dput -f -u ${OSDISTID}-arm64-openio-${REPO} /var/cache/pbuilder/${OSDISTID}-${OSDISTCODENAME}-${ARCH}/result/$(basename ${pkgdsc} .dsc)*.changes
     else
       dput -f -u ${OSDISTID}-openio-${REPO} /var/cache/pbuilder/${OSDISTID}-${OSDISTCODENAME}-${ARCH}/result/$(basename ${pkgdsc} .dsc)*.changes
+    fi
 fi