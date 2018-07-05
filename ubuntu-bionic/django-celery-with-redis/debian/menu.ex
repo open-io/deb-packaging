4e05cdb56db9eff8eed66341c654b45aba6985e1
Vincent Legoll <vincent.legoll@gmail.com>
2018-07-05 09:44:45 +0200

    Remove unused or outdated packages

--- ubuntu-bionic/django-celery-with-redis/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(python-django-celery-with-redis):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="python-django-celery-with-redis" command="/usr/bin/python-django-celery-with-redis"
--- ubuntu-bionic/python-socketio2/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(python-socketio2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="python-socketio2" command="/usr/bin/python-socketio2"
